a795658c8c1d2fb06e1facf9504e64eb6daa403a
Dalton Pettus <daltpettus@gmail.com>
2025-04-19 20:44:47 -0700

    first commit, server and database first steps complete

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+
+// console.log('May Node be with you');
+// console.log(__dirname);
+
+const MongoClient = require('mongodb').MongoClient;
+const connectionString = 'mongodb+srv://daltpettus:Eudaimonia13*@cluster0.sj8b5cc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+MongoClient.connect(connectionString)
+    .then(client => {
+        console.log('Connected to database');
+        const db = client.db('star-wars-quotes');
+        const quotesCollection = db.collection('quotes');
+
+        app.use(express.urlencoded({ extended: true }));
+
+        app.get('/', (req, res) => {
+            res.sendFile(__dirname + '/index.html');
+        })
+
+        app.post('/quotes', (req, res) => {
+            quotesCollection
+                .insertOne(req.body)
+                .then(result => {
+                    console.log(result);
+                    res.redirect('/');
+                })
+                .catch(error => console.error(error));
+        })
+
+        app.listen(3000, () =>  {
+            console.log('listening on 3000');
+        })
+    })
+    .catch(error => console.error(error));
+
+
+
