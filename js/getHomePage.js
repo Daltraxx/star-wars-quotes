81c41f222b3828ff332ddbb206ca776608c73a65
Dalton Pettus <daltpettus@gmail.com>
2025-04-22 12:39:10 -0700

    refactor getHomePage into async function with try catch

--- js/getHomePage.js
+++ js/getHomePage.js
@@ -1,10 +1,11 @@
-const getHomePage = (quotesCollection, res) => {
-    const quotes = quotesCollection.find().toArray()
-        .then(results => {
-            // console.log(results);
-            res.render('index.ejs', { quotes: results });
-        })
-        .catch(error => console.error(error));
+const getHomePage = async(quotesCollection, res) => {
+    try {
+        const results = await quotesCollection.find().toArray();
+        //console.log(results);
+        res.render('index.ejs', { quotes: results });
+    } catch(error) {
+        console.error(error);
+    }
 }
 
 export default getHomePage;
